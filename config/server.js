f5f9e826bcdb8c865e80fa71a505fb9cdd9d0139
maoxiaoke <maoxiaoke@outlook.com>
2021-08-08 00:55:24 +0800

    first init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '065ee51ee1a1c1873068ed3672f36563'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b3b91381-36ee-4c54-bece-dfee9e8a2950'
+};
